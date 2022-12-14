3c219a89098e1a6732bd55203688bb0b86e00d5e
Travis Luong <travis@travisluong.com>
2022-12-13 22:27:00 -0800

    format code

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express()
-const port = 8000
-const { Client } = require('pg');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 8000;
+const { Client } = require("pg");
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
 const client = new Client({
   user: process.env.DB_USER,
@@ -19,49 +19,62 @@ const client = new Client({
 
 client.connect();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.post('/todos', async (req, res) => {
-  const result = await client.query('INSERT INTO todos (name, completed) VALUES ($1, $2) RETURNING *', [req.body.name, req.body.completed])
-  res.send(result.rows)
-})
+app.post("/todos", async (req, res) => {
+  const result = await client.query(
+    "INSERT INTO todos (name, completed) VALUES ($1, $2) RETURNING *",
+    [req.body.name, req.body.completed]
+  );
+  res.send(result.rows);
+});
 
-app.get('/todos', async (req, res) => {
+app.get("/todos", async (req, res) => {
   if (req.query.completed) {
-    const result = await client.query('SELECT * FROM todos WHERE completed = $1', [req.query.completed])
-    res.send(result.rows)
+    const result = await client.query(
+      "SELECT * FROM todos WHERE completed = $1",
+      [req.query.completed]
+    );
+    res.send(result.rows);
   } else {
-    const result = await client.query('SELECT * FROM todos')
-    res.send(result.rows)
+    const result = await client.query("SELECT * FROM todos");
+    res.send(result.rows);
   }
-})
+});
 
-app.get('/todos/:id', async (req, res) => {
-  const result = await client.query('SELECT * FROM todos WHERE id = $1', [req.params.id])
+app.get("/todos/:id", async (req, res) => {
+  const result = await client.query("SELECT * FROM todos WHERE id = $1", [
+    req.params.id,
+  ]);
   if (!result) {
-    res.status(404).send("todo not found")
-    return
+    res.status(404).send("todo not found");
+    return;
   }
-  res.send(result.rows)
-})
+  res.send(result.rows);
+});
 
-app.put('/todos/:id', async (req, res) => {
-  const result = await client.query('SELECT * FROM todos WHERE id = $1', [req.params.id])
+app.put("/todos/:id", async (req, res) => {
+  const result = await client.query("SELECT * FROM todos WHERE id = $1", [
+    req.params.id,
+  ]);
   if (!result) {
-    res.status(404).send("todo not found")
-    return
+    res.status(404).send("todo not found");
+    return;
   }
-  const result2 = await client.query('UPDATE todos SET name = $1, completed = $2 WHERE id = $3 RETURNING *', [req.body.name, req.body.completed, req.params.id])
-  res.send(result2.rows)
-})
+  const result2 = await client.query(
+    "UPDATE todos SET name = $1, completed = $2 WHERE id = $3 RETURNING *",
+    [req.body.name, req.body.completed, req.params.id]
+  );
+  res.send(result2.rows);
+});
 
-app.delete('/todos/:id', async (req, res) => {
-  await client.query('DELETE FROM todos WHERE id = $1', [req.params.id])
-  res.sendStatus(200)
-})
+app.delete("/todos/:id", async (req, res) => {
+  await client.query("DELETE FROM todos WHERE id = $1", [req.params.id]);
+  res.sendStatus(200);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
