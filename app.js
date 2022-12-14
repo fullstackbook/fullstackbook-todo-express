b02a4a89dd9534510b4370483120ddf8b2a2961a
Travis Luong <travis@travisluong.com>
2022-12-13 22:06:29 -0800

    refactor to use pg

--- app.js
+++ app.js
@@ -1,55 +1,63 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-var cors = require('cors')
+const cors = require('cors')
 const app = express()
 const port = 8000
+const { Client } = require('pg');
 
 app.use(bodyParser.json())
 app.use(cors())
 
-const pgp = require('pg-promise')(/* options */)
-const db = pgp('postgres://postgres:@localhost:5432/fullstackbook-todo-express')
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'fullstackbook-todo-express',
+  password: '',
+  port: 5432,
+});
+
+client.connect();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.post('/todos', async (req, res) => {
-  const result = await db.one('INSERT INTO todos (name, completed) VALUES (${name}, ${completed}) RETURNING *', req.body)
-  res.send(result)
+  const result = await client.query('INSERT INTO todos (name, completed) VALUES ($1, $2) RETURNING *', [req.body.name, req.body.completed])
+  res.send(result.rows)
 })
 
 app.get('/todos', async (req, res) => {
   if (req.query.completed) {
-    const result = await db.query('SELECT * FROM todos WHERE completed = $1', [req.query.completed])
-    res.send(result)
+    const result = await client.query('SELECT * FROM todos WHERE completed = $1', [req.query.completed])
+    res.send(result.rows)
   } else {
-    const result = await db.query('SELECT * FROM todos')
-    res.send(result)
+    const result = await client.query('SELECT * FROM todos')
+    res.send(result.rows)
   }
 })
 
 app.get('/todos/:id', async (req, res) => {
-  const result = await db.oneOrNone('SELECT * FROM todos WHERE id = $1', [req.params.id])
+  const result = await client.query('SELECT * FROM todos WHERE id = $1', [req.params.id])
   if (!result) {
     res.status(404).send("todo not found")
     return
   }
-  res.send(result)
+  res.send(result.rows)
 })
 
 app.put('/todos/:id', async (req, res) => {
-  const result = await db.oneOrNone('SELECT * FROM todos WHERE id = $1', [req.params.id])
+  const result = await client.query('SELECT * FROM todos WHERE id = $1', [req.params.id])
   if (!result) {
     res.status(404).send("todo not found")
     return
   }
-  const result2 = await db.one('UPDATE todos SET name = $1, completed = $2 WHERE id = $3 RETURNING *', [req.body.name, req.body.completed, req.params.id])
-  res.send(result2)
+  const result2 = await client.query('UPDATE todos SET name = $1, completed = $2 WHERE id = $3 RETURNING *', [req.body.name, req.body.completed, req.params.id])
+  res.send(result2.rows)
 })
 
 app.delete('/todos/:id', async (req, res) => {
-  await db.none('DELETE FROM todos WHERE id = $1', [req.params.id])
+  await client.query('DELETE FROM todos WHERE id = $1', [req.params.id])
   res.sendStatus(200)
 })
 
